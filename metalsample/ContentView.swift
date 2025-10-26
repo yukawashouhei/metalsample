3ac2522b79211fa7bb266a6a20fe73b34f500805
Shohei <yukawa.shouhei@gmail.com>
2025-10-26 12:54:10 +0900

    Initial Commit

---
+++ metalsample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  metalsample
+//
+//  Created by 湯川昇平 on 2025/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ metalsample/metalsampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  metalsampleApp.swift
+//  metalsample
+//
+//  Created by 湯川昇平 on 2025/10/26.
+//
+
+import SwiftUI
+
+@main
+struct metalsampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
